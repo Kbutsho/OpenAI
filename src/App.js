acc1db6a58538a9d46169b68eefe0fc777109582
Kaushik Biswas <kbutsho@gmail.com>
2023-02-01 01:38:57 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
+import './app.css';
 
 function App() {
   const configuration = new Configuration({
@@ -28,27 +30,30 @@ function App() {
   };
 
   return (
-    <main className="main">
-      <div className="w-2/4 mx-auto">
-        <textarea
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Write your prompt.."
-          className="textarea"
-        ></textarea>
+    <div className="body">
+      <main className="app">
+        <div className="main-area">
+          <textarea
+            type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Write your prompt.."
+            className="textarea"
+          ></textarea>
 
-        <button
-          onClick={handleClick}
-          disabled={loading || prompt.length === 0}
-          className="btn"
-        >
-          {loading ? "Generating..." : "Generate"}
-        </button>
+          <button
+            onClick={handleClick}
+            disabled={loading || prompt.length === 0}
+            className="btn"
+          >
+            {loading ? "Generating..." : "Generate"}
+          </button>
 
-        <pre className="result">{result}</pre>
-      </div>
-    </main>
+          <pre className="result">{result}</pre>
+        </div>
+      </main>
+      <p style={{ color: "white", fontSize: "12px", textAlign:"center" }}>Copyright © {new Date().getFullYear()} || all rights reserved by <a style={{  fontWeight:"bold" }} href="https://kbutsho.netlify.app">kbutsho</a> and OpenAI</p>
+    </div>
   );
 }
 
